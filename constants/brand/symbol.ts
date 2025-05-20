ef02932358a75f9b85da2701aa925e97c57cb90c
Charlotte <charlottewiltshire27@outlook.com>
2025-05-20 15:56:16 +0300

    feat: add branding links and logo/symbol variations for enhanced brand representation

---
+++ constants/brand/symbol.ts
@@ -0,0 +1,27 @@
+export const SYMBOL_VARIATIONS = [
+  {
+    id: "primary",
+    imageSrc: "/brand/symbol/redot-symbol-primary.svg",
+    downloadUrl: "https://image.redotengine.org/Redot_Symbol_Primary.zip",
+    bgColor: "bg-zinc-900",
+    height: 48,
+    width: 48,
+  },
+  {
+    id: "black",
+    imageSrc: "/brand/symbol/redot-symbol-white.svg",
+    downloadUrl: "https://image.redotengine.org/Redot_Symbol_Black.zip",
+    bgColor: "bg-zinc-50",
+    height: 48,
+    width: 48,
+    invert: true,
+  },
+  {
+    id: "white",
+    imageSrc: "/brand/symbol/redot-symbol-white.svg",
+    downloadUrl: "https://image.redotengine.org/Redot_Symbol_White.zip",
+    bgColor: "bg-zinc-900",
+    height: 48,
+    width: 48,
+  },
+];
